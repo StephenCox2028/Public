b4223140b0482a485a7883c359fcd5878c750bd2
Stephen Cox <stevecox2005@gmail.com>
2025-03-12 13:24:34 -0400

    Add files via upload

---
+++ Game Project/WinFormsApp2/WinFormsApp2/Foes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp2
+{
+    internal class Foes:Spells
+    {
+        List<string> names = new List<string>{""};
+        List<string> Keywords = new List<string> { "Raging","Sturdy","Speedy" };
+        (string, string, string, int, int, int, int, string, string, string, string) enemy = ("", "","",0, 0, 0, 0, "", "", "", "");
+    }
+}
---
+++ Game Project/WinFormsApp2/WinFormsApp2/Form1.cs
@@ -0,0 +1,19 @@
+namespace WinFormsApp2
+{
+
+    public partial class Form1 : Form
+    {  
+        static void Mapgeneration()
+        {
+
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+         
+        }
+
+
+    }
+}
